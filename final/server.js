d51e1b73fd37a6bbc0b625ea6588f138bc644b6a
zhousui0209 <zhousui946@gmail.com>
2026-01-07 11:46:26 +0800

    01078

--- final/server.js
+++ final/server.js
@@ -4,7 +4,8 @@ const path = require("path");
 const cors = require("cors");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 app.use(cors());
 app.use(express.json());
